b53d23710e176606fc0a68425a424e4bc1f717ba
henrique1299 <80115091+henrique1299@users.noreply.github.com>
2021-04-21 00:34:42 -0300

    confirm

---
+++ Anagram/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("String 1:");
+        String str1 = scan.next();
+        System.out.println("String 2:");
+        String str2 = scan.next();
+        scan.close();
+
+        Anagram anagram = new Anagram (str1, str2);
+
+        System.out.println("Is Anagram: " + anagram.isAnagram());
+
+    }
+}
